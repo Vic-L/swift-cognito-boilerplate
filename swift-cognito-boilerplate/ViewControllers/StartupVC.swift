82d58d6660324533091567f5ea78c8dc469b39b9
Vic-L <vljc17@gmail.com>
2018-06-07 13:23:09 +0800

    feat: StartupVC

--- swift-cognito-boilerplate/ViewControllers/StartupVC.swift
+++ swift-cognito-boilerplate/ViewControllers/StartupVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartupVC.swift
 //  swift-cognito-boilerplate
 //
 //  Created by Vic-L on 7/6/18.
@@ -8,18 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class StartupVC: UIViewController {
+    // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
