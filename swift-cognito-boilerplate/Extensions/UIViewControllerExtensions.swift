f65aca5ebef4f1bc9bce87c79daab61b4a5465d2
Vic-L <vljc17@gmail.com>
2018-06-07 13:43:01 +0800

    refactor: UIViewControllerExtensions

---
+++ swift-cognito-boilerplate/Extensions/UIViewControllerExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewControllerExtensions.swift
+//  swift-cognito-boilerplate
+//
+//  Created by Vic-L on 7/6/18.
+//  Copyright © 2018 Vic-L. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func goToLoginVC(completion: (((Bool) -> Void)?) = nil) {
+        DispatchQueue.main.async {
+            guard let window = UIApplication.shared.keyWindow else {
+                return
+            }
+            
+            let storyboard: UIStoryboard = UIStoryboard(name: "Authentication", bundle: nil)
+            
+            UIView.transition(with: window, duration: 0.35, options: .transitionFlipFromLeft, animations: {
+                UIApplication.shared.keyWindow?.rootViewController = storyboard.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
+            }, completion: completion)
+        }
+    }
+}
